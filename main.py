5715e863d90e960cb85915fb8662fe0b6015bd3e
tlafu <lafuethibaut@gmail.com>
2026-09-03 16:51:53 +0200

    main

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-# This is a sample Python script.
+from flask import Flask
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return "Hi, PyCharm! This is my VTS server."
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-print ("test")
+    # ⚠️ Écoute sur 0.0.0.0 pour être accessible depuis l'extérieur
+    app.run(host='0.0.0.0', port=3000)
